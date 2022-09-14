25c739b6f8ae8f0bde5a8c177478aea4a5c1d3e3
John Goldsmith <john.goldsmith@hashicorp.com>
2022-09-14 12:09:20 +0200

    working cluster

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ This single node is intended to be used as a building block for other replicatio
 To run this as vault enterprise, add your vault.hclic file to the config directory.
 
 Currently the version selection will go to the `vault_version` variable on line 20 in main.tf. This will change in the future to be called from a module.
+
+# Using on an M1 Mac
+
+```
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.16.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+```
+
+```
+brew install kreuzwerker/taps/m1-terraform-provider-helper
+m1-terraform-provider-helper activate
+m1-terraform-provider-helper install hashicorp/template -v v2.2.0
+```
--- main.tf
+++ main.tf
@@ -1,39 +1,9 @@
 terraform {
   required_version = ">= 0.13"
-}
-
-# -----------------------------------------------------------------------
-# Variables
-# -----------------------------------------------------------------------
-
-# tcp with hostname example:
-# export TF_VAR_docker_host="tcp://docker:2345"
-
-variable "docker_host" {
-  default = "unix:///var/run/docker.sock"
-}
-
-variable "vault_license" {
-  default = ""
-}
-
-variable "vault_version" {
-  default = "1.9.1-ent"
-}
-
-# -----------------------------------------------------------------------
-# Global configuration
-# -----------------------------------------------------------------------
-
-terraform {
   backend "local" {
     path = "tfstate/terraform.tfstate"
   }
 }
-
-# -----------------------------------------------------------------------
-# Provider configuration
-# -----------------------------------------------------------------------
 
 provider "docker" {
   host = var.docker_host
@@ -45,7 +15,7 @@ provider "docker" {
 resource "docker_network" "repl_network" {
   name       = "repl-network"
   attachable = true
-  ipam_config { subnet = "10.42.10.0/24" }
+  ipam_config { subnet = "10.42.10.0/16" }
 }
 
 
@@ -53,27 +23,18 @@ resource "docker_network" "repl_network" {
 # Vault data and resources
 # -----------------------------------------------------------------------
 
-data "template_file" "vault_configuration" {
-  template = file("${path.cwd}/config/vault.hcl")
-}
-
-data "template_file" "vault_license" {
-  template = file("${path.cwd}/config/vault.hclic")
-}
-
 resource "docker_image" "vault" {
   name         = "hashicorp/vault-enterprise:${var.vault_version}"
   keep_locally = true
 }
 
 resource "docker_container" "vault" {
-  name  = "vault"
-  image = docker_image.vault.latest
-  env   = ["SKIP_CHOWN", "VAULT_ADDR=http://127.0.0.1:8200", "VAULT_LICENSE=${var.vault_license}"]
-  # custom binary
-  command = ["vault", "server", "-log-level=trace", "-config=/vault/config"]
-  # command  = ["vault", "server", "-log-level=trace", "-config=/vault/config"]
-  hostname = "vault"
+  count    = 3
+  name     = "vault-${count.index}"
+  image    = docker_image.vault.latest
+  env      = ["VAULT_ADDR=http://127.0.0.1:8200", "VAULT_LICENSE=${var.vault_license}"]
+  command  = ["vault", "server", "-log-level=trace", "-config=/vault/config"]
+  hostname = "vault-${count.index}"
   must_run = true
   capabilities {
     add = ["IPC_LOCK"]
@@ -81,34 +42,38 @@ resource "docker_container" "vault" {
   healthcheck {
     test         = ["CMD", "vault", "status"]
     interval     = "10s"
-    timeout      = "2s"
+    timeout      = "15s"
     start_period = "10s"
-    retries      = 2
+    retries      = 15
   }
   ports {
     internal = "8200"
-    external = "8200"
+    external = format("82%d0", count.index)
     protocol = "tcp"
   }
+  # ports {
+  #   internal = "8201"
+  #   external = format("82%d1", count.index)
+  #   protocol = "tcp"
+  # }
+
   networks_advanced {
     name         = "repl-network"
-    ipv4_address = "10.42.10.200"
+    ipv4_address = "10.42.10.20${count.index}"
   }
+
   upload {
-    content = data.template_file.vault_configuration.rendered
+    content = templatefile("${path.cwd}/config/vault.tftpl", { node_id = count.index })
     file    = "/vault/config/server.hcl"
   }
+
   upload {
-    content = data.template_file.vault_license.rendered
+    content = templatefile("${path.cwd}/config/vault.hclic", {})
     file    = "/vault/config/vault.hclic"
   }
-  # custom vault binary
+
   volumes {
-    host_path      = "${path.cwd}/vault"
-    container_path = "/vault/vault"
-  }
-  volumes {
-    host_path      = "${path.cwd}/vault-audit-log"
-    container_path = "/vault/logs"
+    host_path      = "${path.cwd}/var/raft-${count.index}"
+    container_path = "/var/raft"
   }
 }
